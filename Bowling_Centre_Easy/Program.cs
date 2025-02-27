8428ccf54ae90021349cc681a7dd8130d33b9da9
Viv Madigan <vivyanmadigan@gmail.com>
2025-02-27 08:37:45 +0100

    Starting work on a more modulised UI where user can determine what to do.

--- Bowling_Centre_Easy/Program.cs
+++ Bowling_Centre_Easy/Program.cs
@@ -22,8 +22,8 @@ namespace Bowling_Centre_Easy
             // Note to self BowlingEngine uses laneRepo directly to get lane input from the user.
             BowlingEngine engine = new BowlingEngine(playerService, laneRepo, matchService);
 
-            // Start the game. The engine handles player registration, match creation, etc.
-            engine.StartGame();
+            Console.WriteLine("Welcome to Nackademin Bowling Center");
+
 
             // Keep the console open until a key is pressed.
             Console.WriteLine("Press any key to exit...");
