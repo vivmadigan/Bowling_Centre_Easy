d4e21cd9cc1517b306bcc1b7c96e54c4f6f90f2e
Viv Madigan <vivyanmadigan@gmail.com>
2025-03-02 22:20:11 +0100

    Added better comments using more industry standard syntax. Took away dependency between lane and match repo. Made program more dynamic and also tried to include more error handling.

---
+++ Bowling_Centre_Easy/MenuOptions/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bowling_Centre_Easy.Interfaces;
+
+namespace Bowling_Centre_Easy.MenuOptions
+{
+    public class MainMenu
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public ICommand Command { get; set; }
+    }
+}
